cf01f309e4524133fe6485c4797e46ecfd503731
Carlos Ramos de la Vega <cramosv@berkeley.edu>
2015-03-11 15:36:33 -0700

    BML Simulation Study

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,10 +8,10 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
-  
-   #return(m)
+  m=matrix(data=sample(c(0,1,2),size=r*c,replace=TRUE,prob=c(1-p,p/2,p/2)),nrow=r,ncol=c)
+  return(m)  
 }
+
 
 #### Function to move the system one step (east and north)
 ## Input : a matrix [m] of the same type as the output from bml.init()
@@ -21,16 +21,42 @@ bml.init <- function(r, c, p){
 ## NOTE : the function should move the red cars once and the blue cars once,
 ## you can write extra functions that do just a step north or just a step east.
 
-bml.step <- function(m){
-
+bml.step=function(m){
+  s=m
+  rmstar=cbind(m[,-1],m[,1])
+  rmovers=(m==1)&(rmstar==0)
+  rnon.movers=(m==1)&(rmstar!=0)
+  rmoved.cars=cbind(rmovers[,ncol(m)],rmovers[,1:(ncol(m)-1)])
   
-   #return(list(m, grid.new))
+  m=m*(m==2)+m*rnon.movers+rmoved.cars
+  
+  bmstar=rbind(m[nrow(m),],m[1:(nrow(m)-1),])
+  bmovers=(m==2)&(bmstar==0)
+  bnon.movers=(m==2)&(bmstar!=0)
+  bmoved.cars=rbind(bmovers[2:(nrow(m)),],bmovers[1,])
+  
+  m=m*(m==1)+m*bnon.movers+2*bmoved.cars
+  
+  grid.new=!identical(s,m)
+  
+  return(list(m,grid.new))
 }
-
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
-
 bml.sim <- function(r, c, p){
-
+  sim=bml.init(r,c,p)
+  steps=0
+  for(i in 1:10000){
+    if (bml.step(sim)[[2]]==TRUE){
+      sim= bml.step(sim)[[1]]
+      steps=steps+1}
+    else {
+      break
+    }
+  }
+  return(list(bml.step(sim)[[2]],steps))
 }
+
+
+
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,4 +7,43 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
+simulation.iteration=function(r,c){
+  densities=c(1:20)
+  densities=(5*pvalues)/100
+  matrixx=matrix(data=NA, nrow=100,ncol=20)
+  colnames(matrixx)=densities
+  for (i in 1:20){
+    p=(5*i)/100
+    for(j in 1:100){
+      matrixx[j,i]=bml.sim(r,c,p)[[2]]
+    }}
+  
+  return(matrixx)
+}
 
+
+average.gridlocks=function(x){
+  average.v=c(1:ncol(x))
+  for(i in 1:ncol(x))
+  {average.v[i]=mean(x[,i])}
+  return(average.v)
+}
+
+
+####function for before and after images: 
+
+##FIRST STORE INITIAL  
+initial=bml.init(r,c,p)  
+image.trial=image(initial, col=c("white","red","blue"))
+##THEN RUN CODE TO SEE HOW INITAL ENDS UP     
+bml.sim.images <- function(sim){
+  for(i in 1:10000){
+    if (bml.step(sim)[[2]]==TRUE){
+      sim= bml.step(sim)[[1]]
+    }
+    else {
+      break
+    }
+  }
+  return(image(sim, col=c("white","red","blue")))
+}
