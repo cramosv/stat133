a9c2f341738e772641fdeed0b72300aa3e035442
Carlos Ramos de la Vega <cramosv@berkeley.edu>
2015-03-20 10:51:29 -0700

    Hwk 6 Submission

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -27,19 +27,41 @@
 #                 the entries are 0 for days where the doctor is a
 #                 non-adopter, else 1 (so once a row turns to 1 it stays as 1).
 
-sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
 
-  # Set up the output variable, define it as a matrix then use initial.doctors
-  # to set the first column (day)
+initial.doctors=sample((0:1),size=100,replace=TRUE,prob=c(.9,.1))
 
-  # Run a simulation for <n.days> (use a for loop).  In the loop:
-  # 1) pick two random doctors
-  # 2) check if one has adopted the other hasn't
-  # 3) convert the non-adopter with probability p
+  sim.doctors<- function(initial.doctors, n.doctors, n.days, p){
+    has_adopted=matrix(initial.doctors,nrow=n.doctors, ncol=n.days)
+    for (i in 2:n.days){
+    
+      doc.index=sample(1:n.doctors,size=2,replace=FALSE)
+      if ((runif(1)<p)&sum(has_adopted[doc.index,i-1]==1)){
+        initial.doctors[doc.index]=1
+        has_adopted[,i]=initial.doctors
+      }else{
+        has_adopted[,i]=has_adopted[,i-1]
+      }
+    }
+    
+    return(has_adopted)
+  }
 
-  # return the output
+  a=colSums(sim.doctors(initial.doctors,100,100,.3))
+b=colSums(sim.doctors(initial.doctors,100,100,.5))
+c=colSums(sim.doctors(initial.doctors,100,100,.7))
+d=colSums(sim.doctors(initial.doctors,100,100,.8))
+e=colSums(sim.doctors(initial.doctors,100,100,.999))
+sumary=data.frame(cbind(1:length(initial.doctors),a,b,c,d,e))
+xticks=c(1:100)
+yticks=c(1:(max(sumary[,-1])+10))
 
-}
+plot(x=sumary[,1],y=a,xaxt='n',ylim=c(min(yticks),max(yticks)),col="red", type="l", ylab="Number of  Adopters", xlab="Day", main="New Drug Usage Trends Analysis")
+axis(side=1,at=xticks)
+lines(x=sumary[,1],y=b, col="blue")
+lines(x=sumary[,1],y=c, col="green")
+lines(x=sumary[,1],y=d, col="orange")
+lines(x=sumary[,1],y=e, col="black")
+#HOW TO ADJUST LEGEND!legend("topleft",legend=names(sumary[,-1]), col="red", fill=T, xpd=F)
 
 # When you test your function you have to generate <initial.doctors> and
 # pick values for the other input parameters.
